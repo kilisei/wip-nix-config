26fab2a7b32f5e65407f620def18343743aacf54
Kilisei <seitzkili+git@gmail.com>
2025-03-22 18:59:55 +0100

    chore: cleanup

---
+++ home/kilisei/features/programs/editor/default.nix
@@ -0,0 +1,7 @@
+{
+  imports = [
+    ./nvim.nix
+    ./zed.nix
+    ./vscode.nix
+  ];
+}
---
+++ home/kilisei/style/gtk.nix
@@ -0,0 +1,12 @@
+{
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Adwaita-dark";
+    };
+    iconTheme = {
+      name = "Tela-circle-dark";
+      package = pkgs.tela-circle-icon-theme;
+    };
+  };
+}
